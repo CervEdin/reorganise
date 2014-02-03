3ae14fe77983ca638545d28353dd29e7b149bef2
CervEdin <roadeyes-1@pc3.home>
2014-02-03 19:31:44 +0100

    Clean-up
    Elements generated lowered to 50
    Targeting the appropriate container
    Adding numbering for visualization

--- createElems.js
+++ createElems.js
@@ -1,12 +1,12 @@
-var wrap = $('.main.wrapper');
+var wrap = $('#pinCont');
 function createRandElem(){
     var str = null,
         nr = Math.floor((Math.random()*6)+1);
-    str = '<div class="xrat0' + nr.toString() + '"></div>'
+    str = '<div class="xrat0' + nr.toString() + '"><p>' + arguments[0] + '</p></div>'
     return str;
 }
 $(function() {
-    for(var i=0; i<100; i++){
-    wrap.append(createRandElem());
+    for(var i=0; i<50; i++){
+    wrap.append(createRandElem(i));
     }
 });
