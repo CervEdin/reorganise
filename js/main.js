36385216d3d83176c258e1465d32aa8a743f6d5a
CervEdin <roadeyes-1@pc3.home>
2014-02-03 19:30:00 +0100

    Clean-up
    Bad code removed, code cleaned and comments added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-var organizr = {};
-organizr.blocks = '';
 function ratioKidParent() {
     var kid = arguments[0],
     	childW = kid.offsetWidth,
@@ -13,92 +11,50 @@ function recRatio(){
 	}
 }
 function aryW()	{
-	var arr = arguments[0],
-		cnt = 0;
-		$.each(arr, function(index){
+	var cnt = 0;
+		$.each(arguments[0], function(index){
 			cnt += this.ratio;
 		});
 		return cnt;
 }
 function doesItFit() {
-	var dis = arguments[0],
-		arr = arguments[1];
-		if (ratioKidParent(dis) + aryW(arr) <= 1) {
-			return true;
-		}
-		else {
-		return false;
-		}
+    if (ratioKidParent(arguments[0]) + aryW(arguments[1]) <= 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 function organise() {
 	var id = $(arguments[0]),
-		pins = $(arguments[0] + ' div[class^=xrat]'),
+		pins = $(arguments[0] + ' ' + arguments[1]),
 		ary = [],
 		tempary = [],
 		counter = 0;
-	recRatio($.makeArray(pins));
-	console.log(window.pinColumns);
-	console.log($.makeArray(pins));
+	recRatio(pins);
+//	console.log(pins);
 	pins.each(function(index){
 		if (doesItFit(pins[index], tempary) === true){
-			console.log(1337);
 			tempary.splice(tempary.length, 0, pins[index]);
 			if (aryW(tempary) === 1){
-				console.log(tempary);
+//				console.log(tempary);
 				ary.splice(ary.length, 0, tempary);
 				tempary = [];
 			}
 		}
 		else {
-			console.log(7331);
+//			console.log('Element ' + index + ' didnt fit');
 			for (var i=index; i < pins.length; i++){
 				var dis = pins[i];
+//                console.log('running for loop');
 				if (doesItFit(dis, tempary) === true){
-					console.log(999);
 					tempary.splice(tempary.length, 0, pins[i]);
-					console.log(tempary);
 					if (aryW(tempary) === 1){
 						ary.splice(ary.length, 0, tempary);
 						tempary = [];
-					}
-					else {
-						dis.ratio = 0.33;
 					}
 				}
 			}
 		}
 	});
 }
-$('#button').click(function(){organise('#pinCont');});
-//$(document).ready(organise('#pinCont', 3));
-
-// var rest = columns - counter;
-// 		counter += ratioW($(this))*columns;
-//  		console.log('rest'+rest.toString());
-// 		while (counter > 3) {
-// 			for (var i = index; i < pins.length ; i++){
-// 				var  size = ratioW(pins.eq(i))*columns;
-// 				if (size === rest){
-// 					pins.eq(i).insertBefore(pins.eq(index));
-// 					console.log(i);
-// 					break;
-// 				}
-// 			}
-// 			counter = 0;
-// 		}
-
-// 	pins.each(function(index){
-// 		var rat = this.ratio,
-// 			wid = rat * columns,
-// 			rest = columns - counter - wid;
-// 		if (counter + wid <= columns){
-// 			if (rest < 1 && rest != 0){
-// 				var wif = this.offsetWidth,
-// 					counter + wid + wtf + 1 = rest;
-// 				$(this).width(wif * (rat - wtf));
-// 				wid
-// 			}
-// 			tempary.splice(ary.length, 0, $(this));
-// 			counter += wid;
-// 		}
-// 	});
