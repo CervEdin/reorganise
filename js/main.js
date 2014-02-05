cfe62444d20639d98319ed2d93c8ff4742ce6c39
CervEdin <roadeyes-1@pc3.home>
2014-02-05 18:21:04 +0100

    mo le
    lelele

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Evaluates the ratio of an element relative to its parent
 function ratioKidParent() {
+//    console.log(arguments[0]);
     var kid = arguments[0],
     	childW = kid.offsetWidth,
         parW = kid.parentNode.offsetWidth;
@@ -32,7 +33,8 @@ function doesItFit() {
 // main function, iterates over speciefied DOM elements and attempts to organise them to achieve a ratio of one each 'line'
 function organise() {
 	var id = $(arguments[0]),
-		pins = $(arguments[0] + ' ' + arguments[1]),
+		pins = jQuery.makeArray(jQuery(arguments[0] + ' ' + arguments[1])),
+        store = [],
 		ary = [],
 		tempary = [],
 		counter = 0;
@@ -64,42 +66,60 @@ function organise() {
 //			}
 //		}
 //	});
-    for (var index=0; index < pins.length; index++){
-        console.log(index);
-		if (doesItFit(pins[index], tempary) === true){
-			tempary.splice(tempary.length, 0, pins[index]);
-//            pins.splice(index, 1);
-			if (Math.round(aryW(tempary)*10)/10 === 1){
-//				console.log(tempary);
-				ary.splice(ary.length, 0, tempary);
-				tempary = [];
-			}
-		}
-		else {
-//			console.log('Element ' + index + ' didnt fit');
-			for (var i=index; i < pins.length; i++){
-				var dis = pins[i];
-//                console.log('running for loop');
-				if (doesItFit(dis, tempary) === true){
-					tempary.splice(tempary.length, 0, pins[i]);
-//                    pins.splice(i, 1);
-//					console.log(Math.round(aryW(tempary)*10)/10);
-					if (Math.round(aryW(tempary)*10)/10 === 1){
-						ary.splice(ary.length, 0, tempary);
-						tempary = [];
-					}
-				}
-			}
-		}
-	};
-    console.log(ary);
+//    for (var index=0; index < pins.length; index++){
+//        console.log(index);
+//		if (doesItFit(pins[index], tempary) === true){
+//			tempary.splice(tempary.length, 0, pins[index]);
+////            pins.splice(index, 1);
+//			if (Math.round(aryW(tempary)*10)/10 === 1){
+////				console.log(tempary);
+//				ary.splice(ary.length, 0, tempary);
+//				tempary = [];
+//			}
+//		}
+//		else {
+////			console.log('Element ' + index + ' didnt fit');
+//			for (var i=index; i < pins.length; i++){
+//				var dis = pins[i];
+////                console.log('running for loop');
+//				if (doesItFit(dis, tempary) === true){
+//					tempary.splice(tempary.length, 0, pins[i]);
+////                    pins.splice(i, 1);
+////					console.log(Math.round(aryW(tempary)*10)/10);
+//					if (Math.round(aryW(tempary)*10)/10 === 1){
+//						ary.splice(ary.length, 0, tempary);
+//						tempary = [];
+//					}
+//				}
+//			}
+//		}
+//	};
+    while (pins.length>0){
+        var obj = pins.shift()
+        if (doesItFit(obj, tempary) === true){
+            tempary.push(obj);
+        } else {
+            store.unshift(obj);
+        }
+        if (Math.round(aryW(tempary)*10)/10 === 1){
+            ary = ary.concat(tempary);
+            tempary = [];
+            for (var i=1; i<store.length; i++){
+                var elem = store.shift();
+                pins.unshift(elem);
+            }
+        }
+    }
+    console.log('pins');
+    console.log(pins);
+	console.log('store');
+	console.log(store);
+	console.log('ary');
+	console.log(ary);
     ary.forEach(function(element){
-        element.forEach(function(element){
-            console.log('lol');
             $('#pinTarget').append(element);
         });
-    });
-}
+};
 var original = null;
 $('#organise').click(function(){original = $('.main-container').clone();organise('#pinCont', '[class^="xrat"]');});
 $('#reset').click(function(){$('.main-container').replaceWith(original);});
