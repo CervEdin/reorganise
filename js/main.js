411df2e65ab9772b03620e7fa645e3b70cf7db17
CervEdin <roadeyes-1@pc3.home>
2014-02-12 22:42:42 +0100

    Masonry
    Added masonry for comparison

--- js/main.js
+++ js/main.js
@@ -134,3 +134,4 @@ function organise() {
 };
 $('#organise').click(function(){organise('#pinCont', '[class^="xrat"]');});
 $('#reset').click(function(){$('.main.wrapper').replaceWith(original);});
+$('#masonry').click(function(){var container=document.querySelector('#pinCont');var msnry=new Masonry(container,{itemSelector:'.xrat'});});
