91e3040b7c16ba399e499e7acb76cffb6b0abe1d
CervEdin <roadeyes-1@pc3.home>
2014-02-12 22:41:46 +0100

    syntax
    syntax

--- js/main.js
+++ js/main.js
@@ -96,6 +96,7 @@ function organise() {
 //	};
     while (pins.length>0){
         var obj = pins.shift();
+        if (doesItFit(obj, tempary)){
             tempary.push(obj);
         } else {
             store.unshift(obj);
